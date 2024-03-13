0ef37b21a59c014138642d2238ddbd92ed3559c0
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-13 13:44:56 -0400

    completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
     
-        
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if not type(page_count) == int or page_count < 0:
+            print('page_count must be an integer')
+        else:
+            self._page_count = page_count
+
+    def turn_page(self):
+        print('Flipping the page...wow, you read fast!')
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, condition='Grungy'):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    def __repr__(self):
+        return f'This is the {self.brand} of the night...'
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if type(size) == int and size > 0:
+            self._size = size
+        else:
+            print('size must be an integer')
+        
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self.condition = 'New'
+
